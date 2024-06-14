1a4c72c8a99e783bd7ab55c1d514c24f9497ec8b
RichmondMcCarthy/ <richmondmccarthy9137@gmail.com>
2024-06-14 16:44:12 +0000

    Complete

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDZwfXtop9p8YH3ukW536seKer4oBpF42M",
+    authDomain: "blogsphere-3c2b8.firebaseapp.com",
+    projectId: "blogsphere-3c2b8",
+    storageBucket: "blogsphere-3c2b8.appspot.com",
+    messagingSenderId: "591184385456",
+    appId: "1:591184385456:web:383fa0d3de2640f756b275"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,19 @@
+import flowbite from "flowbite-react/tailwind";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    //'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin(),
+    //require('flowbite/plugin'),
+    require('tailwind-scrollbar'),
+  ],
 }
